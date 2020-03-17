13201645b6dcd3aca1400bd4d254b67411aa5b8d
cnickels21 <cnickels_21@yahoo.com>
2020-03-17 11:57:09 -0500

    new pets constructed and rendered

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-console.log('hi');
+
 var allPets = [];  // Array to hold all pet objects
 
 // Pet constructor
@@ -14,7 +14,7 @@ function Pets(name, imageName, age, weight, isGoodWithKids, isGoodWithDogs, isGo
     this.isGoodWithMail = isGoodWithMail;
     this.personality = personality;
     this.createPet = this.render();
-    Pets.allPets.push(this);
+    allPets.push(this);
 }
 
 Pets.prototype.render = function() {
@@ -64,7 +64,27 @@ Pets.prototype.render = function() {
     article.appendChild(personaParagraph);
 }
 
-new Pets('Norabelle', 'images/doofus.jpg', '3 years', '14 lbs', true, true, true, true, 'Nora is a curious cat, as long as you have kitty snacks.  She is goofy and playful, but also humble and likes here long naps in the sun.');
+// creating new pets here
+new Pets('Norabelle', 'doofus', '3 years', '14 lbs', true, true, true, true, 'Nora is a curious cat, as long as you have kitty snacks.  She is goofy and playful, but also humble and likes here long naps in the sun.');
 
+new Pets('Dexter', 'goofybutt', '4 1/2 years', '74 lbs 8 oz', true, true, true, false, 'Dexter is very proud and energetic.  He requires a lot of time for running throughout your typical week and is a leader through and through.');
 
-// (name, imageName, age, weight, isGoodWithKids, isGoodWithDogs, isGoodWithCats, isGoodWithMail, personality)
+new Pets('Oscar', 'grumpy', '8 years', '12 lbs', false, false, true, false, 'Oscar is dispassionate and disorderly.  He claims his areas as his own and prefers to play with his toys by himself.  As long as he is fed he will take care of himself.');
+
+new Pets('Koa', 'koa', '8 months', '42 lbs', true, true, false, false, 'Koa is extremely energetic and adventurous.  She wants to see the world and wants an owner to share that experience with.  She is also naturally a leader, unless you are leading the way with some butt scratches.');
+
+new Pets('Lady', 'lady', '7 years', '85 lbs', true, true, false, true, 'Lady a gorgeous black German Shepard who is extremely sweet and fun loving. She loves heading to dog parks to find new furry friends but she would be best in a home with no cats. She is warm hearted and loves to spend her evenings snuggling up.');
+
+new Pets('Princess', 'princess', '2 years', '7 lbs', true, false, false, true, 'Princess loves to sleep most of the day and soak up all the affection people have to give her. She is peppy with just the right amount of sass! She  is friendly, chatty, and loves children and would be a great addition to any family home.');
+
+new Pets('Quirky', 'quirky', '10 years', '45 lbs', true, true, true, true, 'Quirky is just that, quirky! She is energetic, bouncy, goofy and very playful. That being said she can handle  just about anything life has to throw at her! Quirky is very intelligent and loves playing fetch.  She loves to greet the postal carrier with plenty of wags and puppy kisses!');
+
+new Pets('River', 'river', '4 years', '3lbs', true, true, false, true, 'River is a sweet little Terrier who loves a great adventure. She is kind, confident and outgoing with just the right amount of spunk. She loves the outdoors but also loves lazy Sundays inside. River prefers homes with no cats but wouldn’t mind another small dog to play with but is perfectly fine on her own.');
+
+new Pets('Skeletor', 'skeletor', '21 years', '8 lbs 4 oz', true, false, true, false, 'Skeletor is old. Old, old, old. But vibrant. So, so, so vibrant. She’s into the relaxed lifestyle. Plenty of pets and plenty of food and She’ll happily own the arm of your couch for the rest of her days.');
+
+new Pets('Rocket', 'sleepy', '7 years', '12 lbs 8 oz', true, false, false, false, 'Rocket is above it all. He’s friendly, but only as far as it serves his needs. He is a hermit king. He is laid back and probably won’t play much. But he may rub against your leg for a morsel of food.');
+
+new Pets('Samwise', 'snowy', '3 years', '60 lbs 8 oz', true, true, true, true, 'Samwise is the perfect dog--fun, loyal, and good-natured. He will never poop on the rug. And he will always play with the children. And the cats. He looooooves cats. And frisbees!');
+
+new Pets('Biff and Buff', 'twins', '2 years', '5 lbs 2 oz and 6 lbs', true, true, true, false, 'Biff and Buff are inseparable siblings. So we insist they be adopted together! They are their own best friends, so worry not about having to give them too much attention. They’re young and curious, so they can be a bit troublesome, but they mean well.');
