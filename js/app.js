78a4421affdd8ab0ae5428ff134115d173a0fbc4
cnickels21 <cnickels_21@yahoo.com>
2020-03-17 11:20:48 -0500

    first pet constructed

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 'use strict';
-
+console.log('hi');
 var allPets = [];  // Array to hold all pet objects
 
 // Pet constructor
-function Pets(name, age, weight) {
-    this.imageName = imageName;
+function Pets(name, imageName, age, weight, isGoodWithKids, isGoodWithDogs, isGoodWithCats, isGoodWithMail, personality) {
     this.name = name;
+    this.imageName = imageName;
     this.age = age;
     this.weight = weight;
     this.isGoodWithKids = isGoodWithKids;
     this.isGoodWithDogs = isGoodWithDogs;
-    this.isGoodWithCats = isGoodwithCats;
+    this.isGoodWithCats = isGoodWithCats;
     this.isGoodWithMail = isGoodWithMail;
     this.personality = personality;
+    this.createPet = this.render();
     Pets.allPets.push(this);
 }
 
@@ -55,7 +56,7 @@ Pets.prototype.render = function() {
     description.appendChild(kitties);
     // good with mailmen or not
     var postal = document.createElement('li');
-    postal.textContent = this.isGoodWithmail;
+    postal.textContent = this.isGoodWithMail;
     description.appendChild(postal);
     // personality paragraph
     var personaParagraph = document.createElement('p');
@@ -63,48 +64,7 @@ Pets.prototype.render = function() {
     article.appendChild(personaParagraph);
 }
 
+new Pets('Norabelle', 'images/doofus.jpg', '3 years', '14 lbs', true, true, true, true, 'Nora is a curious cat, as long as you have kitty snacks.  She is goofy and playful, but also humble and likes here long naps in the sun.');
 
 
-
-// Does well with kids, cats, dogs and mail carriers
-// Personality:  Nora is a curious cat, as long as you have kitty snacks.  She is goofy and playful, but also humble and likes here long naps in the sun.
-
-
-// function Pet(name, breed, imageName, interests, isGoodWithKids, isGoodWithDogs, isGoodWithCats) {
-//     this.name = name;
-//     this.breed = breed;
-//     this.age = this.setAge();
-//     this.imageName = imageName;
-//     this.interests = interests;
-//     this.isGoodWithKids = isGoodWithKids;
-//     this.isGoodWithDogs = isGoodWithDogs;
-//     this.isGoodWithCats = isGoodWithCats;
-//     Pet.allPets.push(this);
-//   }
-//   Pet.Pets = [];
-
-// Create some DOM elements and then render them in the index
-// Pet.prototype.render = function() {
-//     grab the parent element
-//     var parentElement = document.getElementById('kitten-profiles');
-//     create child elements article, h2, p, ul, li interests and image
-//     var article = document.createElement('article');
-//     parentElement.appendChild(article);
-//     var h2 = document.createElement('h2');
-//     h2.textContent = this.name;
-//     article.appendChild(h2);
-//     var petParagraph = document.createElement('p');
-//     petParagraph.textContent = `${this.name} is adorable, and is ${this.age} old.`;
-//     article.appendChild(petParagraph);
-//     var ul = document.createElement('ul');
-//     article.appendChild(ul);
-//     for(let i = 0; i < this.interests.length; i++) {
-//       var li = document.createElement('li');
-//       li.textContent = this.interests[i];
-//       ul.appendChild(li);
-//     }
-//     var img = document.createElement('img');
-//     img.setAttribute('src', 'images/' + this.imageName + '.jpg');
-//     img.setAttribute('alt', 'Adopt ' + this.name + 'NOW!');
-//     article.appendChild(img);
-//   }  
+// (name, imageName, age, weight, isGoodWithKids, isGoodWithDogs, isGoodWithCats, isGoodWithMail, personality)
