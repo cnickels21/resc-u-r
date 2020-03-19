4bc9d6f929f42be6ac32eea21a4e9cf91a12c0c0
cnickels21 <cnickels_21@yahoo.com>
2020-03-19 10:23:45 -0500

    about us rendered

--- js/construct.js
+++ js/construct.js
@@ -51,5 +51,11 @@ new Pets('Jazzy', 'jazzy', '5 years', '13lbs', false, false, true, false, true, 
 
 new Pets('Squatch', 'squatch', '2 1/2 years', '16 lbs', true, false, false, true, false, true, false, 'Squatch can be a handful when you let him get his energy out. He will run laps around the house if you let him. A chaotic bundle of joy and energy, but skiddish around other dogs. You will need to keep your energy levels up to keep up with this little guy, but he will always be there when you need a friend.');
 
+new Pets('Matt Barnhart', 'matthew', '33 years', '180 lbs', false, true, true, true, true, true, true, 'Matt is an aspiring digital market and software developer. He also enjoys coffee, cooking, and connecting with people. He aspires to making positive changes in his community by supporting people to the best of his abilities!');
+
+new Pets('Marie Antons', 'marie', '28', '190 lbs', true, false, true, true, true, true, true, 'Marie Antons is an emerging software developer and student at DeltaV. I recently moved back to Iowa from the small Atlantic Island of Bermuda. I previously was in the hospitality industry as a chef and chose to shift careers to gain more stability while still being able to utilize my creativity and passion for constant learning.');
+
+new Pets('Chase Nickels', 'chase', '28 1/2 years', '160 lbx', false, true, false, true, true, true, true, 'Chase is a laid back individual with an unappeasable wanderlust.  He loves mountains and large bodies of water, as well as music in the park and dancing in the rain.  He grew up in a small town in northern Wisconsin. Software development, he believes, is a stepping stone towards growing within an ever changing industry and world.');
+
 //store the pet objects
 localStorage.setItem('savedPets', JSON.stringify(allPets));
