8f85ae7afd1d4076d4adb932bb7d41778855f413
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-03-19 22:54:54 -0500

    pet roulette! and some styling and formatting.

--- js/home.js
+++ js/home.js
@@ -26,9 +26,100 @@ function inputEmail(e) {
     update.classList.remove('success');
     update.classList.add('failure');
   }
-};
+}
+
+var rouletteButton = document.getElementById('roulette-button');
+rouletteButton.addEventListener('click', petRoulette);
 
 
+var randomInt = Math.floor(Math.random() * allPets.length);
+
+function petRoulette() {
+  var i = randomInt;
+  var hideForm = document.getElementById('form-div');
+  hideForm.classList.add('hide');
+
+  // individual pet profiles
+  var parentElement = document.getElementById('match-profiles');
+  var article = document.createElement('article');
+  parentElement.appendChild(article);
+  // name rendered as card title
+  var title = document.createElement('h2');
+  title.textContent = allPets[i].name;
+  article.appendChild(title);
+  // image set into card
+
+  var cardContainer = document.createElement('div');
+  cardContainer.classList.add('card-container');
+  article.appendChild(cardContainer);
+
+  var card = document.createElement('div');
+  card.classList.add('card');
+  cardContainer.appendChild(card);
+
+  var side = document.createElement('div');
+  side.classList.add('side');
+  card.appendChild(side);
+
+  var img = document.createElement('img');
+  img.setAttribute('src', 'images/' + allPets[i].imageName + '.jpg');
+  img.setAttribute('alt', 'Adopt ' + allPets[i].name + 'NOW!');
+  side.appendChild(img);
+
+  var sideBack = document.createElement('div');
+  sideBack.classList.add('side', 'back');
+  card.appendChild(sideBack);
+
+  var stats = document.createElement('ul');
+  sideBack.appendChild(stats);
+  // age rendered
+  var old = document.createElement('li');
+  old.textContent = `Age: ${allPets[i].age}`;
+  stats.appendChild(old);
+  // weight rendered
+  var heavy = document.createElement('li')
+  heavy.textContent = `Weight: ${allPets[i].weight}`;
+  stats.appendChild(heavy);
+
+  // unordered list for pet description
+  var doggos = document.createElement('li');
+  if (allPets[i].isGoodWithDogs === true) {
+    doggos.textContent = `Enjoys being around dogs`;
+  } else if (allPets[i].isGoodWithDogs === false) {
+    doggos.textContent = `Does not like dogs`;
+  }
+  stats.appendChild(doggos);
+  // good with cats or not
+  var kitties = document.createElement('li');
+  if (allPets[i].isGoodWithCats === true) {
+    kitties.textContent = `Enjoys being around cats`;
+  } else if (allPets[i].isGoodWithCats === false) {
+    kitties.textContent = `Does not like cats`;
+  }
+  stats.appendChild(kitties);
+  // good with kids or not
+  var kids = document.createElement('li');
+  if (allPets[i].isGoodWithKids === true) {
+    kids.textContent = `Enjoys being around kids`;
+  } else if (allPets[i].isGoodWithKids === false) {
+    kids.textContent = `Does not like kids`;
+  }
+  stats.appendChild(kids);
+  // good with mailmen or not
+  var postal = document.createElement('li');
+  if (allPets[i].isGoodWithMail === true) {
+    postal.textContent = `Won't attack postal carriers`;
+  } else if (allPets[i].isGoodWithMail === false) {
+    postal.textContent = `Postal carriers beware!`;
+  }
+  stats.appendChild(postal);
+  // personality paragraph
+  var personaParagraph = document.createElement('p');
+  personaParagraph.textContent = allPets[i].personality;
+  article.appendChild(personaParagraph);
+
+  document.getElementById('petForm').reset();
+}
 
 function handleFormSubmission(event) {
   event.preventDefault();
@@ -48,7 +139,6 @@ function handleFormSubmission(event) {
   hideForm.classList.add('hide');
 
   // localStorage.setItem('userData', JSON.stringify(userData));
-
   for (var i = 0; i < allPets.length; i++) {
     if (userData[0] === allPets[i].energy) {
       if (userData[1] === allPets[i].social) {
@@ -71,28 +161,22 @@ function handleFormSubmission(event) {
     title.textContent = allPets[i].name;
     article.appendChild(title);
     // image set into card
-
     var cardContainer = document.createElement('div');
     cardContainer.classList.add('card-container');
     article.appendChild(cardContainer);
-
     var card = document.createElement('div');
     card.classList.add('card');
     cardContainer.appendChild(card);
-
     var side = document.createElement('div');
     side.classList.add('side');
     card.appendChild(side);
-
     var img = document.createElement('img');
     img.setAttribute('src', 'images/' + allPets[i].imageName + '.jpg');
     img.setAttribute('alt', 'Adopt ' + allPets[i].name + 'NOW!');
     side.appendChild(img);
-
     var sideBack = document.createElement('div');
     sideBack.classList.add('side', 'back');
     card.appendChild(sideBack);
-
     var stats = document.createElement('ul');
     sideBack.appendChild(stats);
     // age rendered
@@ -105,7 +189,6 @@ function handleFormSubmission(event) {
     stats.appendChild(heavy);
 
     // unordered list for pet description
-
     var doggos = document.createElement('li');
     if (allPets[i].isGoodWithDogs === true) {
       doggos.textContent = `Enjoys being around dogs`;
@@ -143,8 +226,6 @@ function handleFormSubmission(event) {
     article.appendChild(personaParagraph);
   }
 
-
-
   document.getElementById('petForm').reset();
 }
 
@@ -167,7 +248,6 @@ window.addEventListener('load', welcomeMessage);
 //Slide show
 // https://stackoverflow.com/questions/50788951/loop-through-image-array-with-pure-javascript
 
-
 (function () {
 
   const imgArray = [];
@@ -176,11 +256,9 @@ window.addEventListener('load', welcomeMessage);
     imgArray.push('../images/' + allPets[i].imageName + '.jpg');
   }
 
-
   var index = 0;  // This will keep track of the current array index to use
   var img = document.getElementById('img'); // Get your reference just once, not on each function call
   console.log(imgArray);
-
 
   function autoChange() {
     // You only need to ensure that the index isn't out of bounds
@@ -196,6 +274,4 @@ window.addEventListener('load', welcomeMessage);
     setTimeout(autoChange, 3000);
   }
   autoChange();
-
 })();
-
