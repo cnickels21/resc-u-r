405a0cda37d758531093a66a05c65ce34f217529
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-03-20 10:19:08 -0500

    styling, polishing, added some text and margins

--- js/home.js
+++ js/home.js
@@ -81,33 +81,33 @@ function petRoulette() {
   // unordered list for pet description
   var doggos = document.createElement('li');
   if (allPets[i].isGoodWithDogs === true) {
-    doggos.textContent = `Enjoys being around dogs`;
+    doggos.textContent = 'Enjoys being around dogs';
   } else if (allPets[i].isGoodWithDogs === false) {
-    doggos.textContent = `Does not like dogs`;
+    doggos.textContent = 'Does not like dogs';
   }
   stats.appendChild(doggos);
   // good with cats or not
   var kitties = document.createElement('li');
   if (allPets[i].isGoodWithCats === true) {
-    kitties.textContent = `Enjoys being around cats`;
+    kitties.textContent = 'Enjoys being around cats';
   } else if (allPets[i].isGoodWithCats === false) {
-    kitties.textContent = `Does not like cats`;
+    kitties.textContent = 'Does not like cats';
   }
   stats.appendChild(kitties);
   // good with kids or not
   var kids = document.createElement('li');
   if (allPets[i].isGoodWithKids === true) {
-    kids.textContent = `Enjoys being around kids`;
+    kids.textContent = 'Enjoys being around kids';
   } else if (allPets[i].isGoodWithKids === false) {
-    kids.textContent = `Does not like kids`;
+    kids.textContent = 'Does not like kids';
   }
   stats.appendChild(kids);
   // good with mailmen or not
   var postal = document.createElement('li');
   if (allPets[i].isGoodWithMail === true) {
-    postal.textContent = `Won't attack postal carriers`;
+    postal.textContent = 'Won\'t attack postal carriers';
   } else if (allPets[i].isGoodWithMail === false) {
-    postal.textContent = `Postal carriers beware!`;
+    postal.textContent = 'Postal carriers beware!';
   }
   stats.appendChild(postal);
   // personality paragraph
@@ -188,33 +188,33 @@ function handleFormSubmission(event) {
     // unordered list for pet description
     var doggos = document.createElement('li');
     if (allPets[i].isGoodWithDogs === true) {
-      doggos.textContent = `Enjoys being around dogs`;
+      doggos.textContent = 'Enjoys being around dogs';
     } else if (allPets[i].isGoodWithDogs === false) {
-      doggos.textContent = `Does not like dogs`;
+      doggos.textContent = 'Does not like dogs';
     }
     stats.appendChild(doggos);
     // good with cats or not
     var kitties = document.createElement('li');
     if (allPets[i].isGoodWithCats === true) {
-      kitties.textContent = `Enjoys being around cats`;
+      kitties.textContent = 'Enjoys being around cats';
     } else if (allPets[i].isGoodWithCats === false) {
-      kitties.textContent = `Does not like cats`;
+      kitties.textContent = 'Does not like cats';
     }
     stats.appendChild(kitties);
     // good with kids or not
     var kids = document.createElement('li');
     if (allPets[i].isGoodWithKids === true) {
-      kids.textContent = `Enjoys being around kids`;
+      kids.textContent = 'Enjoys being around kids';
     } else if (allPets[i].isGoodWithKids === false) {
-      kids.textContent = `Does not like kids`;
+      kids.textContent = 'Does not like kids';
     }
     stats.appendChild(kids);
     // good with mailmen or not
     var postal = document.createElement('li');
     if (allPets[i].isGoodWithMail === true) {
-      postal.textContent = `Won't attack postal carriers`;
+      postal.textContent = 'Won\'t attack postal carriers';
     } else if (allPets[i].isGoodWithMail === false) {
-      postal.textContent = `Postal carriers beware!`;
+      postal.textContent = 'Postal carriers beware!';
     }
     stats.appendChild(postal);
     // personality paragraph
@@ -233,9 +233,9 @@ function welcomeMessage(event) {
   event.preventDefault();
   var returnMessage = localStorage.getItem('yourName');
   if (returnMessage) {
-    pageLoad.textContent = `Welcome back ${returnMessage}.`;
+    pageLoad.textContent = `Welcome back, ${returnMessage}!`;
   } else {
-    pageLoad.textContent = `Welcome to Resc-U-r. Find your ideal pet!`;
+    pageLoad.textContent = 'Welcome to Resc-U-r. Find your ideal pet!';
   }
 }
 
